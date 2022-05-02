014eb37b10db5f6de76c69bb1644a7e8da381a14
zimmerschiedmark <79535287+zimmerschiedmark@users.noreply.github.com>
2022-05-02 11:24:56 -0500

    Integrated scripting from Paul
    Integrated the following:
    Player health and health bar
    Player mana and mana bar
    Enemy counter
    Enemy AI

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,28 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Shooter : MonoBehaviour
 {
 
+    private AudioSource spellSound;
     public Camera cam;
     private Vector3 destination;
     public GameObject projectile;
-    public Transform RHFirePoint;
+    public Transform firePoint;
     public float projectileSpeed = 30;
+    private float timeToFire;
+    public float fireRate = 4;
+    public PlayerStats player;
+    private Coroutine regen;
+    float damage = 25f;
+    private WaitForSeconds regenTic = new WaitForSeconds(.2f);
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spellSound = GetComponent<AudioSource>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButton("Fire1") && Time.time > timeToFire && player.CurrentMana >= 5)
         {
+            spellSound.Play();
+            player.CurrentMana -= 5;
+            timeToFire = Time.time + 1/fireRate;
             ShootProjectile();
+            Shot();
+            if(regen != null)
+            {
+                StopCoroutine(regen);
+            }
+            regen = StartCoroutine(RegenMana());
         }
     }
 
@@ -34,16 +52,42 @@ public class Shooter : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
             destination = hit.point;
         else
-            destination = ray.GetPoint(1000);
+            destination = ray.GetPoint(100);
 
       
-            InstatntiateProjectile(RHFirePoint);
+        InstantiateProjectile(firePoint);
 
     }
 
-    void InstatntiateProjectile(Transform firePoint)
+    void InstantiateProjectile(Transform firePoint)
     {
         var projectileObj = Instantiate(projectile, firePoint.position, Quaternion.identity) as GameObject;
         projectileObj.GetComponent<Rigidbody>().velocity = (destination - firePoint.position).normalized * projectileSpeed;
     }
+
+    void Shot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 100))
+        {
+            EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                enemy.DeductHealth(damage);
+            }
+        }
+    }
+
+    private IEnumerator RegenMana()
+    {
+        yield return new WaitForSeconds(2);
+
+        while (player.CurrentMana < player.MaxMana)
+        {
+            player.CurrentMana += 1;
+            yield return regenTic;
+        }
+        regen = null;
+    }
+
 }
